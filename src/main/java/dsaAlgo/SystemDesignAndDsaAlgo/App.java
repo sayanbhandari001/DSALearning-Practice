0ee291cb8e834c4de253279e0239fda006d75a6c
Sayan Bhandari <sayanbhandari007@gmail.com>
2024-03-19 15:13:28 +0530

    First commit for DSA and Systemdesign practice

---
+++ src/main/java/dsaAlgo/SystemDesignAndDsaAlgo/App.java
@@ -0,0 +1,13 @@
+package dsaAlgo.SystemDesignAndDsaAlgo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
